561dafeeec4bdf90164f6cc68f864f388bcff355
MalyWaldenberg <99757044+MalyWaldenberg@users.noreply.github.com>
2023-07-23 14:56:10 +0300

    add gitignore

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,13 +7,6 @@ import { imagesRouter } from "./images/router";
 dotenv.config();
 
 // App Variables
-
-if (!process.env.PORT) {
-    process.exit(1);
-}
-
-const PORT = parseInt(process.env.PORT as string, 10);
-
 const app = express();
 
 // App Configuration
@@ -22,9 +15,3 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use("/api/images/", imagesRouter);
-
-
-// Server Activation
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
